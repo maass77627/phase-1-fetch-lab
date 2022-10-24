dccbba330e180066cd7555addba920591fc41ff2
Anne Edmondson <edmondson.anne@gmail.com>
2022-10-23 19:32:07 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then ((resp) => resp.json())
+  .then ((jsonData) => renderBooks(jsonData))
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
